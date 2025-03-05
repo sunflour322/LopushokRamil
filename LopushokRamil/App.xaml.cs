4177e9ba9406cc161b56c47791a537b93478e92f
202111 <202111@WINMAC-7>
2025-03-05 15:07:04 +0300

    Добавьте файлы проекта.

---
+++ LopushokRamil/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using LopushokRamil.DB;
+
+namespace LopushokRamil
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static LopushokRamilEntities db = new LopushokRamilEntities();
+    }
+}
---
+++ LopushokRamil/PagesList/ProductListPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LopushokRamil.PagesList
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListPage.xaml
+    /// </summary>
+    public partial class ProductListPage : Page
+    {
+        public ProductListPage()
+        {
+            InitializeComponent();
+            var products = App.db.Product
+               .Include("ProductMaterial.Material") // Загружаем связанные материалы
+               .ToList();
+
+            ProductLv.ItemsSource = products;
+        }
+
+        private void SearchTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ProductLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PageTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
