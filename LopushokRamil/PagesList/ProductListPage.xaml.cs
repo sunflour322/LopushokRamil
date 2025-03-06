82343574b49753df0d9bdd92095bb7dfc7785c92
sunflour322 <myasnoypelmesh@gmail.com>
2025-03-06 11:05:21 +0300

    asdasd

--- LopushokRamil/PagesList/ProductListPage.xaml.cs
+++ LopushokRamil/PagesList/ProductListPage.xaml.cs
@@ -20,19 +20,62 @@ namespace LopushokRamil.PagesList
     /// </summary>
     public partial class ProductListPage : Page
     {
+        public int pageNumber = 1;
+        public int countPages = 0;
+        public int result;
         public ProductListPage()
         {
             InitializeComponent();
-            var products = App.db.Product
-               .Include("ProductMaterial.Material") // Загружаем связанные материалы
+            pageNumberTb.Text = pageNumber.ToString();
+            ProductLv.ItemsSource = App.db.Product
+               .Include("ProductMaterial.Material").OrderBy(x => x.ID).Skip(result).Take(20)// Загружаем связанные материалы
                .ToList();
-
-            ProductLv.ItemsSource = products;
+             if(App.db.Product.Count() % 20 == 0)
+             {
+                countPages = App.db.Product.Count() / 20;
+             }
+            else
+            {
+                countPages = App.db.Product.Count() / 20 + 1;
+            }
+                
         }
 
         private void SearchTb_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            ProductLv.ItemsSource = App.db.Product.Include("ProductMaterial.Material").OrderBy(x => x.ID).Skip(result).Take(20).Where(i =>
+               i.Name.StartsWith(SearchTb.Text))// Загружаем связанные материалы
+               .ToList();
+        }
+        private void LeftClick(object sender, RoutedEventArgs e)
+        {
+            pageNumber --;
+            if (pageNumber < 1)
+            {
+                MessageBox.Show("Начало списка");
+                pageNumber ++;
+                return;
+            }
+            pageNumberTb.Text = pageNumber.ToString();
+            result -= 20;
+            ProductLv.ItemsSource = App.db.Product
+              .Include("ProductMaterial.Material").OrderBy(x => x.ID).Skip(result).Take(20)// Загружаем связанные материалы
+              .ToList();
+        }
+        private void RightClick(object sender, RoutedEventArgs e)
+        {
+            pageNumber++;
+            if (pageNumber > countPages)
+            {
+                MessageBox.Show("Конец списка");
+                pageNumber--;
+                return;
+            }
+            pageNumberTb.Text = pageNumber.ToString();
+            result += 20;
+            ProductLv.ItemsSource = App.db.Product
+              .Include("ProductMaterial.Material").OrderBy(x => x.ID).Skip(result).Take(20)// Загружаем связанные материалы
+              .ToList();
         }
 
         private void ProductLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -40,9 +83,6 @@ namespace LopushokRamil.PagesList
 
         }
 
-        private void PageTb_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
+        
     }
 }
